a2d07c4c6bfdf5cf6b32aec5b9cd2f8153a52e79
happiness_m <2789140037@qq.com>
2020-04-20 22:20:32 +0800

    登录

--- src/main.js
+++ src/main.js
@@ -4,6 +4,19 @@ import router from './router'
 import store from './store'
 import './plugins/element.js'
 
+//导入api的全局函数
+import { postKeyValueRequest, getRequest, postRequest,putRequest, deleteRequest} from './utils/api'
+
+Vue.prototype.postKeyValueRequest = postKeyValueRequest;
+Vue.prototype.getRequest = getRequest;
+Vue.prototype.postRequest = postRequest;
+Vue.prototype.putRequest = putRequest;
+Vue.prototype.deleteRequest = deleteRequest;
+// 导入全局样式
+import './assets/css/global.css'
+// 导入iconfont字体图标库
+import './assets/fonts/iconfont.css'
+
 Vue.config.productionTip = false
 
 new Vue({
